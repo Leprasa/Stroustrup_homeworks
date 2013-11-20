6ea680c05443ca8aa80768b710683d723bef1eb2
leprasa <zuxel15325076@gmail.com>
2013-11-20 16:26:02 +0400

    error

--- task3.cpp
+++ task3.cpp
@@ -1,15 +1,12 @@
 #include <iostream>
 int main()
 {
-	void f()
-	{
-		inf jd[3];
-		jd[1].month = 'september';
-		jd[1].days = '30';
-		jd[2].month = 'october';
-		jd[2].days = '31';
-		jd[3].month = 'november';
-		jd[3].days = '31';
+		address jd[3];
+		jd[1].month = "september";
+		jd[1].days = "30";
+		jd[2].month = "october";
+		jd[2].days = "31";
+		jd[3].month = "november";
+		jd[3].days = "31";
 		std::cout << jd[1] << '\n' << jd[2] << '\n' << jd[3] << '\n'
-	}
 }
