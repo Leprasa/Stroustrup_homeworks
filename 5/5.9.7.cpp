b4744b80437b9b4a1d57a0e9c3e0e80cc161b2d7
leprasa <zuxel15325076@gmail.com>
2013-11-27 21:33:22 +0400

    7

--- 5/5.9.7.cpp
+++ 5/5.9.7.cpp
@@ -1,15 +1,34 @@
 //7.Определите таблицу, содержащую название месяцев и кол-во дней в каждом из них.
 //Выведите содержимое таблицы.Воспользуйтесб массивом структур,каждая из которых 
 //хранит назв. месяца и кол-во дней в нем. 
+//
 #include <iostream>
+
+struct month {
+	const char *name;
+	int days;
+};
+
 int main()
 {
-		address jd[3];
-		jd[1].month = "september";
-		jd[1].days = "30";
-		jd[2].month = "october";
-		jd[2].days = "31";
-		jd[3].month = "november";
-		jd[3].days = "31";
-		std::cout << jd[1] << '\n' << jd[2] << '\n' << jd[3] << '\n'
+	month year[5];
+
+	year[0].name = "Jan";
+	year[0].days = 31;
+
+	year[1].name = "Feb";
+	year[1].days = 30;
+
+	year[2].name = "Mar";
+	year[2].days = 31;
+
+	year[3].name = "Apr";
+	year[3].days = 30;
+
+	year[4].name = "May";
+	year[4].days = 31;
+
+	for (int i = 0; i < 5; i++) {
+		std::cout << year[i].name << " " << year[i].days << std::endl;
+	}
 }
